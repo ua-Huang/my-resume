c90aa868cb22c050b93d56a114badca2e5bef2f9
ua-Huang <huangzihao289@gmail.com>
2025-04-14 13:39:13 +0800

    all

--- app.py
+++ app.py
@@ -50,7 +50,10 @@ if __name__ == '__main__':
         rendered_html = render_template('index.html', resume=RESUME_DATA)  # 直接渲染
 
     # 保存到 index.html
-    with open("index.html", "w", encoding="utf-8") as f:
-        f.write(rendered_html)
+    with open("index.html", "r+", encoding="utf-8") as f:
+        content = f.read()
+        content = content.replace("{{ url_for('static', filename='images/profile.jpg') }}", "static/images/profile.jpg")
+        f.seek(0)
+        f.write(content)
 
     print("✅ 静态 HTML 已生成：index.html")
